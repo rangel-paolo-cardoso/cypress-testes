cb3e686601342c5b5269676965c7b4fad6f7f4d9
Rangel Paolo <paolorangel1996@outlook.com>
2021-08-27 15:32:34 -0300

    Making login test

--- cypress/integration/sample_test.js
+++ cypress/integration/sample_test.js
@@ -1,23 +1,23 @@
 /* eslint-disable no-undef */
-describe('Visit the website and confirm', () => {
-  it('Visits and do a log in', () => {
-    // Visits Zapelino
+describe('Visit the website', () => {
+  it('Check elements', () => {
     cy.visit('https://zapelino-hmg.contazap.com/');
 
-    // Verify screen elements
-    cy.get('h5').contains('Conta Zap');
-
+    cy.get('h5').contains('Conta Zap');    
     cy.get('label[for=id__input__email]').contains('Email');
-
     cy.get('label[for=id__input__password]').contains('Senha secreta');
-
     cy.get('label[for=id__input__companyId]').contains('Empresa');
-
     cy.get('a').contains(' Esqueceu a senha?');
-
     cy.get('button[type=submit]').contains('Entrar');
   });
-
+  
+  it('Login', () => {
+    cy.get('input[name=email]').type('rangel.bomfim@contazap.com');
+    cy.get('input[name=password]').type('12345678k');
+    cy.get('div.css-1hwfws3').click();
+    cy.get('div.css-1cegrgq').click();
+    cy.get('button[type=submit]').click();
+  });
   //
   // cy.get('button').contains('Change the text value below');
   // cy.get('p').contains('Developed by:');
