97cccacd4171ee8c95e572a7ab16cb3d023dd56c
Rangel Paolo <paolorangel1996@outlook.com>
2021-08-27 15:22:17 -0300

    Checking Conta Zap elements

--- cypress/integration/sample_test.js
+++ cypress/integration/sample_test.js
@@ -1,12 +1,27 @@
 /* eslint-disable no-undef */
-describe('LOgin test', () => {
+describe('Visit the website and confirm', () => {
   it('Visits and do a log in', () => {
-    cy.visit('https://zapelino-hmg.contazap.com/')
-    cy.get('h5').contains('Conta Zap');
-    // cy.get('button').contains('Change the text value below');
-    // cy.get('p').contains('Developed by:');
+    // Visits Zapelino
+    cy.visit('https://zapelino-hmg.contazap.com/');
 
-    // cy.get('button').click();
-    // cy.get('p').contains('Developed by: Rangel Paolo');
+    // Verify screen elements
+    cy.get('h5').contains('Conta Zap');
+
+    cy.get('label[for=id__input__email]').contains('Email');
+
+    cy.get('label[for=id__input__password]').contains('Senha secreta');
+
+    cy.get('label[for=id__input__companyId]').contains('Empresa');
+
+    cy.get('a').contains(' Esqueceu a senha?');
+
+    cy.get('button[type=submit]').contains('Entrar');
   });
+
+  //
+  // cy.get('button').contains('Change the text value below');
+  // cy.get('p').contains('Developed by:');
+
+  // cy.get('button').click();
+  // cy.get('p').contains('Developed by: Rangel Paolo');
 });
