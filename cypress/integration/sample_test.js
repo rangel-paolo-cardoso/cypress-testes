48c7daa9ff35bdb7b31e849f1eb7ff96ca433b39
Rangel Paolo <paolorangel1996@outlook.com>
2021-08-27 14:57:13 -0300

    Visiting Conta Zap

--- cypress/integration/sample_test.js
+++ cypress/integration/sample_test.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
-describe('My first test', () => {
-  it('VIsits the kitchen sink', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('h2').contains('Hello World!');
-    cy.get('button').contains('Change the text value below');
-    cy.get('p').contains('Developed by:');
+describe('LOgin test', () => {
+  it('Visits and do a log in', () => {
+    cy.visit('https://zapelino-hmg.contazap.com/')
+    cy.get('h5').contains('Conta Zap');
+    // cy.get('button').contains('Change the text value below');
+    // cy.get('p').contains('Developed by:');
 
-    cy.get('button').click();
-    cy.get('p').contains('Developed by: Rangel Paolo');
+    // cy.get('button').click();
+    // cy.get('p').contains('Developed by: Rangel Paolo');
   });
 });
